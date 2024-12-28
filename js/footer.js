2f278bbd52897ef8e59a19d3fd0f478d8ec38f82
cambl <cam.mclagan@gmail.com>
2024-12-27 21:10:28 -0800

    more stuff!

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,4 @@
-fetch('/html/templates/footer.html')
+fetch('../templates/footer.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('footer').innerHTML = data;
